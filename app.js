b4d6406c9ce52ce4d884c69a8ca593d2fb5c3985
Johnnie Hård <johnnie@synvinkel.org>
2018-08-04 10:51:14 +0200

    get image and pipe it

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const compression = require('compression')
 const logger = require('morgan')
+const https = require('https')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -43,8 +44,58 @@ function runApp() {
 
     app.get('/timeseries', checkApiKey, timeseries)
 
-    app.get('/image', checkApiKey, (req, res, next) => {
-        // get image script
+    app.get('/image/:lng/:lat/:filename', (req, res, next) => {
+        try {
+            const { lng, lat, filename } = req.params
+
+            const index = filename.split('.')[0]
+
+            const coords = [parseFloat(lng), parseFloat(lat)]
+            // error handling for coordinates
+            if (coords.some((coord, i) => {
+                if (!coord) {
+                    return true
+                }
+                if (i === 0 && (coord > 180 || coord < -180)) {
+                    return true
+                }
+                if (i === 1 && (coord > 90 || coord < -90)) {
+                    return true
+                }
+                return false
+            })) {
+                next({
+                    message: "Please provide proper lat/lng"
+                })
+                return
+            }
+
+            const aoi = ee.Geometry.Point(coords).buffer(2000).bounds()
+
+            ee.Image(`COPERNICUS/S2/${index}`).clip(aoi)
+                .visualize({
+                    bands: ['B4', 'B3', 'B2'],
+                    min: 0, max: 2500
+                })
+                .getThumbURL({
+                    dimensions: 1000,
+                    format: 'png'
+                }, (url, err) => {
+                    if (err) {
+                        next({
+                            message: err
+                        })
+                        return
+                    }
+
+                    https.get(url, file => file.pipe(res))
+                })
+
+
+
+        } catch (e) {
+            next(e)
+        }
     }
     )
 
