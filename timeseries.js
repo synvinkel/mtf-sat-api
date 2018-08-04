b4ce3e38ed9e4276e0776ae1784033ad1599b743
Johnnie Hård <johnnie@synvinkel.org>
2018-08-04 08:55:56 +0200

    error handling for coordinates

--- timeseries.js
+++ timeseries.js
@@ -5,8 +5,28 @@ module.exports = (req, res, next) => {
     try {
 
         const { lat, lng } = req.query
+        const coords = [parseFloat(lat), parseFloat(lng)]
 
-        const poi = ee.Geometry.Point([parseFloat(lat), parseFloat(lng)])
+        // error handling for coordinates
+        if (coords.some((coord, i) => {
+            if (!coord) {
+                return true
+            }
+            if (i === 0 && (coord > 90 || coord < -90)) {
+                return true
+            }
+            if (i === 1 && (coord > 180 || coord < -180)) {
+                return true
+            }
+            return false
+        })) {
+            next({
+                message: "Please provide proper lat/lng"
+            })
+            return
+        }
+
+        const poi = ee.Geometry.Point()
         const s2 = ee.ImageCollection('COPERNICUS/S2')
             .filterBounds(poi)
 
